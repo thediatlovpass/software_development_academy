fc7c2444e01997bc092ef7e11766e4972f91b79e
lucas <lucas.rusiecki@gmail.com>
2018-02-03 14:34:00 +0100

    Added: counting sort

---
+++ sort-exercises/src/main/java/com/sda/sorters/countingsort/CountingSort.java
@@ -0,0 +1,12 @@
+package com.sda.sorters.countingsort;
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+        Integer[] inputArray = {6, 3, 6, 1, 4, 9, 0, 1, 8, 2, 6, 4, 9, 3, 7, 5, 9, 2, 7, 3, 2, 4, 1, 8, 7, 0, 8, 5, 8, 3, 6, 2, 5, 3};
+    }
+
+    public static void countingSort(Integer[] args) {
+
+    }
+}
