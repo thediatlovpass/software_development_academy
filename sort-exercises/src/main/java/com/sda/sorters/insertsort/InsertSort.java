7887b7fc555162f218021c479f73c44890278f0c
lucas <lucas.rusiecki@gmail.com>
2018-02-03 14:30:03 +0100

    Added: insert sort - sort letters

--- sort-exercises/src/main/java/com/sda/sorters/insertsort/InsertSort.java
+++ sort-exercises/src/main/java/com/sda/sorters/insertsort/InsertSort.java
@@ -2,10 +2,48 @@ package com.sda.sorters.insertsort;
 
 import com.sda.sorters.AbstractSort;
 
+import java.util.Arrays;
+
 public class InsertSort extends AbstractSort {
     public static void main(String[] args) {
-        Integer[] inputArray = {7, 3, 4, 8, 5, 9};
-        insertSort(inputArray, false);
+//        Integer[] inputArray = {7, 3, 4, 8, 5, 9};
+//        insertSort(inputArray, false);
+        String[] inputArray = {"ca", "cd", "cb", "a", "cba", "cda", "acd"};
+        insertSortString(inputArray);
+    }
+
+    public static void insertSortString(String[] array) {
+        Integer pointer = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < pointer; j++) {
+                char[] arrayJ = array[j].toCharArray();
+                char[] arrayP = array[pointer].toCharArray();
+                for (int k = 0; k < arrayJ.length && k < arrayP.length; k++) {
+                    if (arrayJ[k] > arrayP[k]) {
+                        swapString(array, pointer, j);
+                        break;
+                    }
+                    if (arrayJ[k] < arrayP[k]) break;
+                    if (arrayJ[k] == arrayP[k]) {
+                        if (arrayP.length == k + 1) {
+                            swapString(array, pointer, j);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (pointer < array.length - 1) {
+                pointer++;
+            }
+        }
+    }
+
+    private static void swapString(String[] array, Integer pointer, int j) {
+        System.out.println(Arrays.asList(array).toString());
+        System.out.println(array[pointer] + " <= " + array[j]);
+        String temp = array[pointer];
+        array[pointer] = array[j];
+        array[j] = temp;
     }
 
     public static void insertSort(Integer[] array, boolean orderAsc) {
