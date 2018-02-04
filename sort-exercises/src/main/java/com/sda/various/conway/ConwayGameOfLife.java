88efcb9da662baa5fcbacb2938f0ddaec9dbadbb
lucas <lucas.rusiecki@gmail.com>
2018-02-04 13:54:51 +0100

    Added: conwey game of life

--- sort-exercises/src/main/java/com/sda/various/conway/ConwayGameOfLife.java
+++ sort-exercises/src/main/java/com/sda/various/conway/ConwayGameOfLife.java
@@ -17,9 +17,53 @@ public class ConwayGameOfLife extends JPanel {
 
     public ConwayGameOfLife(int width, int height) {
         this.grid = new int[width / 4][height / 4];
+        init();
+    }
+
+    private void init() {
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                grid[j][i] = getRandom();
+            }
+        }
+    }
+
+    private int getRandom() {
+        double random = Math.random();
+        return random > 0.92 ? 1 : 0;
     }
 
     public void updateGrid() {
+        int[][] tempArray = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                tempArray[j][i] = isAlive(i, j);
+            }
+        }
+        grid = tempArray;
+    }
+
+    private int isAlive(int posX, int posY) {
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int newPosX = posX + i;
+                int newPosY = posY + j;
+                if (newPosX >= 0 && newPosX < grid[0].length
+                        && newPosY >= 0 && newPosY < grid.length
+                        && grid[newPosY][newPosX] == 1) {
+                    if (i == 0 && j == 0) continue;
+                    count++;
+                }
+            }
+        }
+        if (count == 3 && grid[posY][posX] == 0) {
+            return 1;
+        } else if (grid[posY][posX] == 1 && count >= 2 && count <= 3) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     @Override
@@ -55,7 +99,6 @@ public class ConwayGameOfLife extends JPanel {
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
         new Timer(100, new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 c.updateGrid();
