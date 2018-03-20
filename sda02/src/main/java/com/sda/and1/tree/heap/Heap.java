bf374f4f4dd525c49663f42b83e8b97a7ed23d5c
lucas <lucas.rusiecki@gmail.com>
2018-03-20 21:04:29 +0100

    Added: heap add, remove

--- sda02/src/main/java/com/sda/and1/tree/heap/Heap.java
+++ sda02/src/main/java/com/sda/and1/tree/heap/Heap.java
@@ -11,13 +11,63 @@ public class Heap {
         int index = values.size();
         values.add(value);
         int parentIndex = calculateParentIndex(index);
-        while(index > 0 && values.get(parentIndex) < value){
+        while (index > 0 && values.get(parentIndex) < value) {
             Integer parentValue = values.get(parentIndex);
             values.set(index, parentValue);
             index = parentIndex;
             parentIndex = calculateParentIndex(index);
         }
         values.set(index, value);
+    }
+
+    public Integer remove() {
+        int size = values.size();
+        if (size == 0) return 0;
+        Integer currentRoot = values.get(0);
+        int currentIndex = 0;
+        int lastValue = values.get(size - 1);
+        values.remove(size - 1);
+        values.set(currentIndex, lastValue);
+        int greatestChildIndex = getGreatestChildIndex(currentIndex);
+        while (greatestChildIndex > 0 && values.get(greatestChildIndex) > lastValue) {
+            Integer greatestChildValue = values.get(greatestChildIndex);
+            Integer currentValue = values.get(currentIndex);
+            values.set(currentIndex, greatestChildValue);
+            values.set(greatestChildIndex, currentValue);
+            currentIndex = greatestChildIndex;
+            greatestChildIndex = getGreatestChildIndex(currentIndex);
+        }
+        return currentRoot;
+    }
+
+    public int getGreatestChildIndex(int index) {
+        int leftIndex = index * 2 + 1;
+        if (leftSonExists(index) && rightSonExists(index)) {
+            Integer leftChild = values.get(leftIndex);
+            int rightIndex = index * 2 + 2;
+            Integer rightChild = values.get(rightIndex);
+            if (leftChild > rightChild) {
+                return leftIndex;
+            } else {
+                return rightIndex;
+            }
+        }
+        if (leftSonExists(index)) {
+            return leftIndex;
+        }
+        return -1;
+    }
+
+    public boolean leftSonExists(int index) {
+        return (2 * index + 1) < values.size();
+    }
+
+    public boolean rightSonExists(int index) {
+        return (2 * index + 2) < values.size();
+    }
+
+    public boolean isLeaf(int index) {
+        return (2 * index + 2) > values.size();
     }
 
     public int calculateParentIndex(int index) {
--- sda02/src/main/java/com/sda/and1/tree/heap/Main.java
+++ sda02/src/main/java/com/sda/and1/tree/heap/Main.java
@@ -5,20 +5,21 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Heap heap = new Heap();
+        heap.add(25);
         heap.add(12);
         heap.add(11);
-        heap.add(9);
         heap.add(8);
         heap.add(10);
         heap.add(6);
-        heap.add(3);
+        heap.add(9);
         heap.add(6);
         heap.add(7);
         heap.add(9);
         heap.add(3);
         heap.add(5);
         heap.add(4);
+        heap.add(3);
 
-        heap.add(25);
+        heap.remove();
     }
 }
