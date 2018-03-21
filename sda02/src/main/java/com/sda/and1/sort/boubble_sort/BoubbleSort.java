8103a72da51b508a2f348e87e4562288a40b1f74
lucas <lucas.rusiecki@gmail.com>
2018-03-21 21:09:53 +0100

    Added: bubble sort

---
+++ sda02/src/main/java/com/sda/and1/sort/boubble_sort/BoubbleSort.java
@@ -0,0 +1,38 @@
+package com.sda.and1.sort.boubble_sort;
+
+import java.util.Arrays;
+
+public class BoubbleSort {
+    public static void main(String[] args) {
+        Integer[] array = {3, 4, 5, 2, 1};
+        bubbleSort(array);
+        System.out.println(Arrays.asList(array));
+    }
+
+    private static void bubbleSort(Integer[] array) {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    isSorted = false;
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            if(isSorted){
+                break;
+            }
+            isSorted = true;
+        }
+//        for (int j = 0; j < array.length - 1; j++) {
+//            if (array[j] > array[j + 1]) {
+//                int temp = array[j + 1];
+//                array[j + 1] = array[j];
+//                array[j] = temp;
+//                bubbleSort(array);
+//            }
+//        }
+
+    }
+}
