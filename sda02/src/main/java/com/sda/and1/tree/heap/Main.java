0a9df16e4e08dd1802219edc70a86def44d8f8e9
lucas <lucas.rusiecki@gmail.com>
2018-03-21 18:55:14 +0100

    Added: heap sort static #1

--- sda02/src/main/java/com/sda/and1/tree/heap/Main.java
+++ sda02/src/main/java/com/sda/and1/tree/heap/Main.java
@@ -46,9 +46,29 @@ public class Main {
             array[index] = valueToInsert;
             heapSize++;
         }
-        System.out.println(array);
-        System.out.println(Arrays.asList(array));
         //robierz kopiec
+        heapSize = array.length;
+        int size = heapSize;
+        for (int i = 0; i < size; i++) {
+            Integer currentRoot = array[0];
+            int currentIndex = 0;
+            int lastValue = array[heapSize - 1];
+            if (heapSize > 0) {
+                array[currentIndex] = lastValue;
+                int greatestChildIndex = getGreatestChildIndex(array, heapSize, currentIndex);
+                while (greatestChildIndex > 0 && array[greatestChildIndex] > lastValue) {
+                    Integer greatestChildValue = array[greatestChildIndex];
+                    Integer currentValue = array[currentIndex];
+                    array[currentIndex] = greatestChildValue;
+                    array[greatestChildIndex] = currentValue;
+                    currentIndex = greatestChildIndex;
+                    greatestChildIndex = getGreatestChildIndex(array, heapSize, currentIndex);
+                }
+            }
+            array[size - i - 1] = currentRoot;
+            heapSize--;
+        }
+        System.out.println(Arrays.asList(array));
     }
 
     public static int calculateParentIndex(int index) {
@@ -56,5 +76,31 @@ public class Main {
             return 0;
         }
         return Math.floorDiv(index - 1, 2);
+    }
+
+    public static int getGreatestChildIndex(Integer[] array, int heapSize, int index) {
+        int leftIndex = index * 2 + 1;
+        if (leftSonExists(heapSize, index) && rightSonExists(heapSize, index)) {
+            Integer leftChild = array[leftIndex];
+            int rightIndex = index * 2 + 2;
+            Integer rightChild = array[rightIndex];
+            if (leftChild > rightChild) {
+                return leftIndex;
+            } else {
+                return rightIndex;
+            }
+        }
+        if (leftSonExists(heapSize, index)) {
+            return leftIndex;
+        }
+        return -1;
+    }
+
+    public static boolean leftSonExists(int heapSize, int index) {
+        return (2 * index + 1) < heapSize;
+    }
+
+    public static boolean rightSonExists(int heapSize, int index) {
+        return (2 * index + 2) < heapSize;
     }
 }
