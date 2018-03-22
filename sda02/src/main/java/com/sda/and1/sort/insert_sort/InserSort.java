5abf4944a91e2ed8da0f81571898c42fc55513a7
lucas <lucas.rusiecki@gmail.com>
2018-03-22 19:36:30 +0100

    Add: insert sort strings

--- sda02/src/main/java/com/sda/and1/sort/insert_sort/InserSort.java
+++ sda02/src/main/java/com/sda/and1/sort/insert_sort/InserSort.java
@@ -6,12 +6,28 @@ public class InserSort {
 
     public static void main(String[] args) {
 
-        Integer[] array = {8, 5, 4, 9, 2, 6};
-        insertSort(array);
-        System.out.println(Arrays.asList(array));
+//        Integer[] array = {8, 5, 4, 9, 2, 6};
+//        insertSort(array);
+        String[] inputArray = {"c", "cb", "ca", "a", "Bc", "bcaa"};
+        insertSortString(inputArray);
+        System.out.println(Arrays.asList(inputArray));
     }
 
     public static void insertSort(Integer[] array) {
+        int pointer = 1;
+        for(int i = 1; i < array.length; i++) {
+            int val = array[i];
+            for(int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertSortString(String[] array) {
         int pointer = 1;
         for(int i = 1; i < array.length; i++) {
             int val = array[i];
