7696849da732774b189428222e7fe1d75f975ec1
lucas <lucas.rusiecki@gmail.com>
2018-03-23 19:09:10 +0100

    Add: maze stack

--- sda02/src/main/java/com/sda/and1/various/MazeSolver.java
+++ sda02/src/main/java/com/sda/and1/various/MazeSolver.java
@@ -1,5 +1,7 @@
 package com.sda.and1.various;
 
+import java.util.Stack;
+
 public class MazeSolver {
     public static void main(String[] args) {
         int[][] maze1 = {
@@ -15,7 +17,16 @@ public class MazeSolver {
                 {1, 1, 0, 1, 0, 1},
                 {1, 1, 1, 1, 0, 1}
         };
-        solveMaze(maze1);
+
+//        Stack<Point> points = new Stack<>();
+//        points.push(new Point(1, 1));
+//        points.push(new Point(2, 1));
+//        points.push(new Point(1, 4));
+//        points.push(new Point(16, 1));
+
+//        System.out.println(isExist(new Point(1, 4), points));
+//        solveMaze(maze1);
+        solveMaze(maze2);
 
     }
 
@@ -23,13 +34,43 @@ public class MazeSolver {
         int i = 0;
         while (maze1[i] != 0) {
             i++;
-        }return i;
+        }
+        return i;
     }
 
     public static void solveMaze(int[][] maze1) {
+        Stack<Point> points = new Stack<>();
         int x = findStart(maze1[0]);
         int y = 0;
-        System.out.println(y + " " + x);
+        Point currentPoint = new Point(x, y);
+        while (currentPoint.getY() != maze1.length - 1) {
+            System.out.println(currentPoint.getY() + " " + currentPoint.getX());
+            if (maze1[currentPoint.getY() + 1][currentPoint.getX()] == 0 && maze1[currentPoint.getY() + 1][currentPoint.getX()] != 2) {
+                currentPoint.setY(currentPoint.getY() + 1);
+                points.push(new Point(currentPoint));
+            } else if (maze1[currentPoint.getY()][currentPoint.getX() + 1] == 0) {
+                currentPoint.setX(currentPoint.getX() + 1);
+                points.push(new Point(currentPoint));
+            } else if (maze1[currentPoint.getY()][currentPoint.getX() - 1] == 0) {
+                currentPoint.setX(currentPoint.getX() - 1);
+                points.push(new Point(currentPoint));
+            } else {
+                maze1[currentPoint.getY()][currentPoint.getX()] = 2;
+                currentPoint.setY(currentPoint.getY() - 1);
+                points.push(new Point(currentPoint));
+            }
+
+        }
+
+        System.out.println(currentPoint.getY() + " " + currentPoint.getX());
     }
 
+
+    public static Point isExist(Point point, Stack<Point> points) {
+        Point foundPoint = points.stream()
+                .filter(point1 -> point1.getY() == point.getY() && point1.getX() == point.getX())
+                .findAny()
+                .get();
+        return foundPoint;
+    }
 }
