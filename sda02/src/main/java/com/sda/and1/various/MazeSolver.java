33c02ed30cf147f5f9b9e2ae417c57537ba1e273
lucas <lucas.rusiecki@gmail.com>
2018-03-23 18:02:24 +0100

    Add: bootstrap files for exercises

--- sda02/src/main/java/com/sda/and1/various/MazeSolver.java
+++ sda02/src/main/java/com/sda/and1/various/MazeSolver.java
@@ -15,5 +15,21 @@ public class MazeSolver {
                 {1, 1, 0, 1, 0, 1},
                 {1, 1, 1, 1, 0, 1}
         };
+        solveMaze(maze1);
+
     }
+
+    public static int findStart(int[] maze1) {
+        int i = 0;
+        while (maze1[i] != 0) {
+            i++;
+        }return i;
+    }
+
+    public static void solveMaze(int[][] maze1) {
+        int x = findStart(maze1[0]);
+        int y = 0;
+        System.out.println(y + " " + x);
+    }
+
 }
