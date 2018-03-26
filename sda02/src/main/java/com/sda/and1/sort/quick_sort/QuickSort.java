4c10d81f1231343ea9fcdd15d3c3a55468d11ce8
lucas <lucas.rusiecki@gmail.com>
2018-03-26 18:41:10 +0200

    quickSort

--- sda02/src/main/java/com/sda/and1/sort/quick_sort/QuickSort.java
+++ sda02/src/main/java/com/sda/and1/sort/quick_sort/QuickSort.java
@@ -1,4 +1,34 @@
 package com.sda.and1.sort.quick_sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] arrayToSort = {7,4,3,5,8,2,3};
+        quickSort(arrayToSort, 0, arrayToSort.length -1);
+        System.out.println(Arrays.toString(arrayToSort));
+    }
+
+    private static void quickSort(int[] arrayToSort, int left, int right) {
+        int pivotIndex = (left + right) / 2;
+        int pivot = arrayToSort[pivotIndex];
+        arrayToSort[pivotIndex] = arrayToSort[right];
+        int j = left;
+        for (int i = left; i < right; i++) {
+            if ( arrayToSort[i] < pivot){
+                int tmp = arrayToSort[i];
+                arrayToSort[i]= arrayToSort[j];
+                arrayToSort[j] = tmp;
+                j++;
+            }
+        }
+        arrayToSort[right] = arrayToSort[j];
+        arrayToSort[j] = pivot;
+        if (left < j - 1){
+            quickSort(arrayToSort, left, j -1);
+        }
+        if (j + 1 < right){
+            quickSort(arrayToSort, j +1, right);
+        }
+    }
 }
