7c8dc01f19281513a60b74bc36d64927b77c471d
lucas <lucas.rusiecki@gmail.com>
2018-03-27 19:27:20 +0200

    Added: letter game #1

--- sda02/src/main/java/com/sda/and1/various/letters/Main.java
+++ sda02/src/main/java/com/sda/and1/various/letters/Main.java
@@ -1,11 +1,20 @@
 package com.sda.and1.various.letters;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Main {
     // regex "^(?!.*?(.).*?\\1)[" + SLOWO + "]*[" + SLOWO + "]*$"
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         List<String> words = dictionary.getWords();
+
+        List<String> filterWord = words.stream().filter(s -> s.length() >= 5).collect(Collectors.toList());
+        Random random=new Random();
+        String randomWord=filterWord.get(random.nextInt(filterWord.size()));
+
+
+
     }
 }
