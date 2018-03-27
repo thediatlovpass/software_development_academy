d9a4e88ecd4b340bac7d46ba60211b1906d21c10
lucas <lucas.rusiecki@gmail.com>
2018-03-27 21:03:41 +0200

    Added: MaxSubbarraySum

--- sda02/src/main/java/com/sda/and1/various/MaxSubarraySum.java
+++ sda02/src/main/java/com/sda/and1/various/MaxSubarraySum.java
@@ -1,5 +1,9 @@
 package com.sda.and1.various;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MaxSubarraySum {
     //    MaxSubarraySum.max(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
     //      returns "6: {4, -1, 2, 1}"
@@ -8,6 +12,25 @@ public class MaxSubarraySum {
     }
 
     private static String max(int[] input) {
-        return null;
+
+        List<Integer> resultArray = new ArrayList<>();
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int j = 0; j < input.length; j++) {
+            List<Integer> tmpArray = new ArrayList<>();
+            int sum = input[j];
+            tmpArray.add(sum);
+
+
+            for (int i = j + 1; i < input.length; i++) {
+                sum += input[i];
+                tmpArray.add(input[i]);
+                if (maxSum < sum) {
+                    maxSum = sum;
+                    resultArray = new ArrayList<>(tmpArray);
+                }
+            }
+        }
+        return resultArray.toString();
     }
 }
