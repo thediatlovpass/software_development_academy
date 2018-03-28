ca78f1b00a04f8330c2899e0054c5e30e7fb3090
lucas <lucas.rusiecki@gmail.com>
2018-03-28 17:00:30 +0200

    Added: exercises bootstrap #2

--- sda02/src/main/java/com/sda/and1/various/ReversePolishNotation.java
+++ sda02/src/main/java/com/sda/and1/various/ReversePolishNotation.java
@@ -3,6 +3,7 @@ package com.sda.and1.various;
 import org.assertj.core.api.Assertions;
 
 public class ReversePolishNotation {
+    // https://pl.wikipedia.org/wiki/Odwrotna_notacja_polska
     //expression 5 1 2 + 4 * + 3 - (which is equivalent to 5 + ((1 + 2) * 4) - 3 in normal notation) should evaluate to 14
     public static void main(String[] args) {
         Assertions.assertThat(calculate("5 1 2 + 4 * + 3")).isEqualTo(14);
