2452c82a3dbafacac30c7d57f1a342628497239c
lucas <lucas.rusiecki@gmail.com>
2018-03-28 18:33:10 +0200

    Added:next bigger number solution

--- sda02/src/main/java/com/sda/and1/various/NextBiggerNumberSameDigits.java
+++ sda02/src/main/java/com/sda/and1/various/NextBiggerNumberSameDigits.java
@@ -2,17 +2,34 @@ package com.sda.and1.various;
 
 import org.assertj.core.api.Assertions;
 
+import java.util.Arrays;
+
 public class NextBiggerNumberSameDigits {
     public static void main(String[] args) {
         Assertions.assertThat(nextBiggerNumber(12)).isEqualTo(21);
         Assertions.assertThat(nextBiggerNumber(513)).isEqualTo(531);
-        Assertions.assertThat(nextBiggerNumber(2017)).isEqualTo(2071);
+        Assertions.assertThat(nextBiggerNumber(2017)).isEqualTo(7210);
         Assertions.assertThat(nextBiggerNumber(9)).isEqualTo(-1);
         Assertions.assertThat(nextBiggerNumber(111)).isEqualTo(-1);
         Assertions.assertThat(nextBiggerNumber(531)).isEqualTo(-1);
     }
 
     private static int nextBiggerNumber(int number) {
-        return -1;
+//        char[] num =    // String.valueOf(number);
+        char[] chars = String.valueOf(number).toCharArray();
+        char[] sortedArray = Arrays.copyOf(chars, chars.length);
+        Arrays.sort(sortedArray);  // (Integer)(x, y) -> (x > y) ? -1 : ((x == y) ? 0 : 1));
+//        Object[] objects = String.valueOf(number)
+//                .chars()
+//                .mapToObj(value -> ((Integer) value))
+//                .sorted((x, y) -> (x < y) ? 1 : ((x == y) ? 0 : -1))
+//                .map(integer -> ((Integer)integer));
+
+        for (int i = sortedArray.length - 1; i >= 0; i--) {
+            chars[i] = sortedArray[sortedArray.length - 1 - i];
+        }
+        int sortedNumber = Integer.parseInt(String.valueOf(chars));
+
+        return (sortedNumber==number)? -1 : sortedNumber;
     }
 }
