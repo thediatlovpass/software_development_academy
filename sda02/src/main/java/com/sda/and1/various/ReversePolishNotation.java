dfdc3583a544f580b1393b99e8ddcf3d0752e9a7
lucas <lucas.rusiecki@gmail.com>
2018-03-29 18:14:17 +0200

    Added: reverse polish fix

--- sda02/src/main/java/com/sda/and1/various/ReversePolishNotation.java
+++ sda02/src/main/java/com/sda/and1/various/ReversePolishNotation.java
@@ -6,7 +6,7 @@ public class ReversePolishNotation {
     // https://pl.wikipedia.org/wiki/Odwrotna_notacja_polska
     //expression 5 1 2 + 4 * + 3 - (which is equivalent to 5 + ((1 + 2) * 4) - 3 in normal notation) should evaluate to 14
     public static void main(String[] args) {
-        Assertions.assertThat(calculate("5 1 2 + 4 * + 3")).isEqualTo(14);
+        Assertions.assertThat(calculate("5 1 2 + 4 * + 3 -")).isEqualTo(14);
     }
 
     private static int calculate(String input) {
