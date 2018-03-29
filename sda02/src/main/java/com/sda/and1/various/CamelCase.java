864aad580535709ba9f0abac121f859ebf84bb53
lucas <lucas.rusiecki@gmail.com>
2018-03-29 18:06:40 +0200

    Added: camel case

--- sda02/src/main/java/com/sda/and1/various/CamelCase.java
+++ sda02/src/main/java/com/sda/and1/various/CamelCase.java
@@ -1,5 +1,7 @@
 package com.sda.and1.various;
 
+import java.util.Arrays;
+
 public class CamelCase {
     // returns "theStealthWarrior"
 //    toCamelCase("the-stealth-warrior")
@@ -9,9 +11,22 @@ public class CamelCase {
     public static void main(String[] args) {
         System.out.println(toCamelCase("the-stealth-warrior"));
         System.out.println(toCamelCase("The_Stealth_Warrior"));
+        System.out.println(toCamelCase("The Stealth Warrior"));
     }
 
     private static String toCamelCase(String input) {
-        return null;
+        String[] sentence = input.split("_|-| ");
+        String result ="";
+        for (int i = 1; i < sentence.length; i++) {
+            char[] temp = sentence[i].toCharArray();
+            if (temp[0] >= 'a' && temp[0] <= 'z') {
+                temp[0] -= 32;
+            }
+            sentence[i] = String.valueOf(temp);
+        }
+            for(String string : sentence){
+                result+=string;
+            }
+          return result;
     }
 }
