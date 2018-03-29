f7325ab4528d778e9d8141cbe7a0f864dfde6562
lucas <lucas.rusiecki@gmail.com>
2018-03-29 18:59:06 +0200

    Added: reverse polish solution

--- sda02/src/main/java/com/sda/and1/various/ReversePolishNotation.java
+++ sda02/src/main/java/com/sda/and1/various/ReversePolishNotation.java
@@ -1,15 +1,47 @@
 package com.sda.and1.various;
 
-import org.assertj.core.api.Assertions;
+import java.util.Stack;
 
 public class ReversePolishNotation {
-    // https://pl.wikipedia.org/wiki/Odwrotna_notacja_polska
-    //expression 5 1 2 + 4 * + 3 - (which is equivalent to 5 + ((1 + 2) * 4) - 3 in normal notation) should evaluate to 14
+
     public static void main(String[] args) {
-        Assertions.assertThat(calculate("5 1 2 + 4 * + 3 -")).isEqualTo(14);
+
+        String exp = "5 1 2 + 4 * + 3 -";
+        String exp2 = "12 2 3 4 * 10 5 / + * +";
+
+        System.out.println(revert(exp));
+        System.out.println(revert(exp2));
     }
 
-    private static int calculate(String input) {
-        return 0;
+    private static double revert(String exp) {
+
+        Stack<Double> stack = new Stack<>();
+        String[] expArr = exp.split("\\s+");
+
+        for (String e : expArr) {
+            if (e.matches("\\d+")) {
+                stack.push(Double.valueOf(e));
+            } else {
+                double a = stack.pop();
+                double b = stack.pop();
+
+                switch (e) {
+                    case "+":
+                        stack.push(b + a);
+                        break;
+                    case "-":
+                        stack.push(b - a);
+                        break;
+                    case "*":
+                        stack.push(b * a);
+                        break;
+                    case "/":
+                        stack.push(b / a);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
     }
 }
