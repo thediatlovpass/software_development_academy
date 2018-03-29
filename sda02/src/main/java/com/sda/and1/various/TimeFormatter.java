c827bb1c99ab45c7f0e55609869f1d6a1bb37907
lucas <lucas.rusiecki@gmail.com>
2018-03-29 20:18:33 +0200

    Added: TimeFormatter solution

--- sda02/src/main/java/com/sda/and1/various/TimeFormatter.java
+++ sda02/src/main/java/com/sda/and1/various/TimeFormatter.java
@@ -3,12 +3,28 @@ package com.sda.and1.various;
 import org.assertj.core.api.Assertions;
 
 public class TimeFormatter {
+    public static int[] units = {60, 3600, 86400};
+    public static String[] names = {"minute", "hour", "day"};
+
     public static void main(String[] args) {
         Assertions.assertThat(format(62)).isEqualTo("1 minute and 2 seconds");
         Assertions.assertThat(format(3662)).isEqualTo("1 hour, 1 minute and 2 seconds");
     }
 
     private static String format(int seconds) {
-        return null;
+        StringBuilder resultText = new StringBuilder();
+        for (int i = units.length - 1; i >= 0; i--) {
+            if (seconds > units[i]) {
+                int count = seconds / units[i];
+                seconds -= count * units[i];
+                resultText.append(count).append(" ").append(names[i]);
+                if (i > 0) {
+                    resultText.append(", ");
+
+                }
+            }
+        }
+        resultText.append(" and ").append(seconds).append(" seconds");
+        return resultText.toString();
     }
 }
