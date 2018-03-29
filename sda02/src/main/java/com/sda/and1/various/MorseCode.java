1fe52653dc2f9b758d54c0953d3484a7cf010332
lucas <lucas.rusiecki@gmail.com>
2018-03-29 19:51:29 +0200

    Added: Morse code solution

--- sda02/src/main/java/com/sda/and1/various/MorseCode.java
+++ sda02/src/main/java/com/sda/and1/various/MorseCode.java
@@ -1,5 +1,7 @@
 package com.sda.and1.various;
 
+import java.util.Arrays;
+
 public class MorseCode {
     //MorseCode.decode(".... . -.--   .--- ..- -.. .")
     // "HEY JUDE"
@@ -17,14 +19,44 @@ public class MorseCode {
             "-----", "--..--", ".-.-.-", "..--.."};
 
     public static void main(String[] args) {
+        System.out.println(decode(".... . -.--  .--- ..- -.. ."));
+        System.out.println(encode("HEY JUDE"));
 
     }
 
     public static String decode(String input) {
-        return "";
+        String[] array = input.split(" ");
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < morse.length; i++) {
+                if (morse[i].equals(array[j])) {
+                    array[j] = String.valueOf(signs[i]);
+                } else if (array[j].equals("")) {
+                    array[j] = " ";
+                    j++;
+                }
+            }
+        }
+        String.join("", array);
+//        String result ="";
+//        for (int i = 0; i < array.length; i++) {
+//            result += array[i];
+//        }
+        return String.join("", array);
     }
 
     public static String encode(String input) {
-        return "";
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < signs.length; j++) {
+                if (input.toLowerCase().charAt(i) == signs[j]) {
+                    text.append(morse[j]).append(" ");
+                }
+            }
+            if (input.charAt(i) == ' '){
+                text.append(" ");
+            }
+        }
+
+        return text.toString();
     }
 }
