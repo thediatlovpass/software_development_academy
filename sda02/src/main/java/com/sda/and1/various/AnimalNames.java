4b97985b6b686275b7455f36a5edbe3e5b987dd2
lucas <lucas.rusiecki@gmail.com>
2018-04-04 20:04:52 +0200

    Added: animal names #1

--- sda02/src/main/java/com/sda/and1/various/AnimalNames.java
+++ sda02/src/main/java/com/sda/and1/various/AnimalNames.java
@@ -1,5 +1,7 @@
 package com.sda.and1.various;
 
+import org.assertj.core.api.Assertions;
+
 public class AnimalNames {
     /*
     Give you some ```chars```, and they can combine many animal names.
@@ -20,11 +22,15 @@ public class AnimalNames {
             "hen", "mole", "duck", "goat"};
 
     public static void main(String[] args) {
-//        sc("dogcat")=2("dog" and "cat")
-//        sc("bcaatt")=2("bat" and "cat")
-//        sc("dogdog")=2("dog" and "dog")
-//        sc("dopig")=1(only contains one of "dog" or "pig")
-//        sc("goatcode")=2  should return 2("dog","cat"), not 1("goat")
-//        sc("cockdogwdufrbir")=4  should return 4("cow","duck","frog","bird") not 2("cock" and "dog")
+        Assertions.assertThat(count("dogcat")).isEqualTo(2); // =2("dog" and "cat")
+        Assertions.assertThat(count("bcaatt")).isEqualTo(2); // =2("bat" and "cat")
+        Assertions.assertThat(count("dogdog")).isEqualTo(2); // =2("dog" and "dog")
+        Assertions.assertThat(count("dopig")).isEqualTo(1); // =1(only contains one of "dog" or "pig")
+        Assertions.assertThat(count("goatcode")).isEqualTo(2); // =2  should return 2("dog","cat"), not 1("goat")
+        Assertions.assertThat(count("cockdogwdufrbir")).isEqualTo(4); // =4  should return 4("cow","duck","frog","bird") not 2("cock" and "dog")
+    }
+
+    private static int count(String input) {
+        return 0;
     }
 }
